0c794d13b005c49f4bad503992a5fa86da62072a
alkiscmr-edu <cemrealkis@posta.mu.edu.tr>
2025-05-09 11:58:56 +0300

    Add files via upload

---
+++ StackLinkedList.java
@@ -0,0 +1,28 @@
+public class StackLinkedList implements Stack {
+
+    StackItem top;
+
+    @Override
+    public void push(Object Item) {
+        StackItem newBox = new StackItem(Item);
+        StackItem previousTop = top;
+    }
+
+    @Override
+    public Object pop() {
+        Object tempData = top.getData();
+        top = top.getNext();
+        return tempData;
+    }
+
+    @Override
+    public void peek() {
+        System.out.println("The top is " + this.top);
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
